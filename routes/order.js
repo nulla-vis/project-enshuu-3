7cf5025cb0a7eea1f9cceb835ff85a244cc8844e
aanusemahu <aanusemahu@gmail.com>
2021-10-22 20:21:45 +0700

    fix bug on order route

--- routes/order.js
+++ routes/order.js
@@ -5,20 +5,12 @@ const axios = require('axios')
 const ORDER_API_URL = 'http://127.0.0.1:8080/api/orders'
 const MENU_API_URL = 'http://menu-env.eba-5hsjxsvv.us-west-2.elasticbeanstalk.com/api/menus'
 
-
-// router.all('/order_api/', (req, res) => {
-//     console.log('masuk api gateaway')
-//     axios.get(`${ORDER_API_URL}`).then((response) => {
-//         res.send(response.data)
-//     })
-// })
 router.post('/create/', (req, res) => {
     axios.post(`${ORDER_API_URL}/create/`, req.body,{
         headers: {'Content-Type': 'application/json'}
     }).then((response) => {
-        res.status(200).send({"message": "request masuk"})
+        res.status(200).send(response.data)
     })
-    // res.send({"message": req.body.lalala})
 })
 
 module.exports = router
