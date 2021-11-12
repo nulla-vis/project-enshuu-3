abb2c84337c6a98830cb4005e37d8f485cb7eb2b
aanusemahu <aanusemahu@gmail.com>
2021-11-12 14:09:11 +0700

    add cors policy and bodyparser

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 const express = require('express')
+const bodyParser = require("body-parser");
+const cors=require("cors");
+
+
 const app = express()
+
+
 const PORT = 3000
+const corsOptions ={
+   origin:'*', 
+   credentials:true, //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions))
+
+// middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json())
+
 
 // import routes
 const menuRoutes = require('./routes/menu')
 const orderRoutes = require('./routes/order')
 
-// middleware
-app.use(express.json())
+app.post("/test/new", (req, res) => {
+    console.log(req.body)
+
+    res.status(201).send({"message": 'ok'})
+})
+
 
 // endpoints
 app.use('/menu', menuRoutes)
--- routes/order.js
+++ routes/order.js
@@ -2,15 +2,21 @@
 const express = require('express')
 const router = express.Router()
 const axios = require('axios')
-const ORDER_API_URL = 'http://127.0.0.1:8080/api/orders'
+const ORDER_API_URL = 'http://projectenshuuorder-env.eba-6vrtphy3.us-east-2.elasticbeanstalk.com/api/orders'
 const MENU_API_URL = 'http://menu-env.eba-5hsjxsvv.us-west-2.elasticbeanstalk.com/api/menus'
 
+
 router.post('/create/', (req, res) => {
-    axios.post(`${ORDER_API_URL}/create/`, req.body,{
-        headers: {'Content-Type': 'application/json'}
-    }).then((response) => {
-        res.status(200).send(response.data)
-    })
+  console.log(req.body)
+      axios.post(`${ORDER_API_URL}/create/`, req.body,{
+       headers: {'Content-Type': 'application/json'}
+   })
+   .then((response) => {
+       res.status(200).send(response.data)
+   })
+
 })
 
 module.exports = router
+
+ 
