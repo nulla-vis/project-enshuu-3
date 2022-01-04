a1ff98cf72cd1d3bdc4e7bc65bae3b0c3ae54b5a
aanusemahu <aanusemahu@gmail.com>
2022-01-04 14:17:53 +0700

    refactoring, fix socket.io connection problem(page keep refresh when connect)

--- app/socket.js
+++ app/socket.js
@@ -6,60 +6,79 @@ const MENU_LIST_API_URL = 'http://menu-env.eba-5hsjxsvv.us-west-2.elasticbeansta
 exports.initialize = function(server) {
 
     let io = require('socket.io')(server, {cors: {origin: "*"}})
-
-
+    
+    
     io.on('connection', function(socket){
-        let order_data = []
-        let order_id_array = []
-        axios.get(`${ORDER_API_URL}/incoming/`).then((response) => {
-            order_data.push(response.data)
-            order_data[0].forEach(data => {
-                data.menu_title = "pending..."
-                data.menu_image = "pending..."
-                order_id_array.push(data.menu_id)
-            })
-            axios.post(`${MENU_LIST_API_URL}`, {"id_list" : `"${order_id_array}"`},{
-                headers: {'Content-Type': 'application/json'}
-            })
-            .then((response) => {
-                order_data[0].forEach(data => {
-                    response.data.menus.forEach(menu => {
-                        if(data.menu_id === menu.id) {
-                            data.menu_title = menu.title
-                            data.menu_image = menu.menu_image
-                        }
-                    })
-                })
-                io.emit('getAllOrder', {order : order_data[0]})
-            })
-        })
-        
-
-        socket.on('newOrder', () => {
+        try {
             let order_data = []
             let order_id_array = []
             axios.get(`${ORDER_API_URL}/incoming/`).then((response) => {
                 order_data.push(response.data)
                 order_data[0].forEach(data => {
-                    data.menu_title = "pending..."
-                    data.menu_image = "pending..."
+                    // data.menu_title = "pending..."
+                    // data.menu_image = "pending..."
                     order_id_array.push(data.menu_id)
                 })
-                axios.post(`${MENU_LIST_API_URL}`, {"id_list" : `"${order_id_array}"`},{
-                    headers: {'Content-Type': 'application/json'}
-                })
-                .then((response) => {
-                    order_data[0].forEach(data => {
-                        response.data.menus.forEach(menu => {
-                            if(data.menu_id === menu.id) {
-                                data.menu_title = menu.title
-                                data.menu_image = menu.menu_image
-                            }
-                        })
+                if(order_id_array.length != 0 ) {
+                    axios.post(`${MENU_LIST_API_URL}`, {"id_list" : `"${order_id_array}"`},{
+                        headers: {'Content-Type': 'application/json'}
                     })
-                    socket.broadcast.emit('incomingOrder',{order : order_data[0]})
-                })
-            })
+                    .then((response) => {
+                        order_data[0].forEach(data => {
+                            response.data.menus.forEach(menu => {
+                                if(data.menu_id === menu.id) {
+                                    data.menu_title = menu.title
+                                    data.menu_image = menu.menu_image
+                                }
+                            })
+                        })
+                        socket.emit('getAllOrder', {order : order_data[0]})
+                    }).catch(function () {
+                        console.log("Socket-Connection-post Promise Rejected");
+                   });
+                }
+            }).catch(function () {
+                console.log("Socket-Connection-get Promise Rejected");
+           });
+        } catch (error) {
+            console.error(`Error received from axios.post: ${JSON.stringify(err)}`);
+        }
+
+        
+
+        socket.on('newOrder', () => {
+            try {
+                let order_data = []
+                let order_id_array = []
+                axios.get(`${ORDER_API_URL}/incoming/`).then((response) => {
+                    order_data.push(response.data)
+                    order_data[0].forEach(data => {
+                        data.menu_title = "pending..."
+                        data.menu_image = "pending..."
+                        order_id_array.push(data.menu_id)
+                    })
+                    axios.post(`${MENU_LIST_API_URL}`, {"id_list" : `"${order_id_array}"`},{
+                        headers: {'Content-Type': 'application/json'}
+                    })
+                    .then((response) => {
+                        order_data[0].forEach(data => {
+                            response.data.menus.forEach(menu => {
+                                if(data.menu_id === menu.id) {
+                                    data.menu_title = menu.title
+                                    data.menu_image = menu.menu_image
+                                }
+                            })
+                        })
+                        socket.broadcast.emit('incomingOrder',{order : order_data[0]})
+                    }).catch(function () {
+                        console.log("Socket-newOrder-post Promise Rejected");
+                   });
+                }).catch(function () {
+                    console.log("Socket-newOrder-get Promise Rejected");
+               });
+            } catch (error) {
+                console.error(`Error received from axios.post: ${JSON.stringify(err)}`);
+            }
             
         })
 
